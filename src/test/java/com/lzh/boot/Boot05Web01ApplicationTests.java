93e440c7cf6dd77fe21f637f17853da480b75181
liangzhanheng <1255371190@qq.com>
2022-03-27 21:03:04 +0800

    hot-fix commit3

--- src/test/java/com/lzh/boot/Boot05Web01ApplicationTests.java
+++ src/test/java/com/lzh/boot/Boot05Web01ApplicationTests.java
@@ -12,6 +12,7 @@ class Boot05Web01ApplicationTests {
         System.out.println("hello,git2");
         System.out.println("hello,git3");
         System.out.println("hello,git4");
+        System.out.println("hotfix,test");
     }
 
 }
